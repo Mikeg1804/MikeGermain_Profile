38bcb8e975c1e678ed81183d5d089c6e18d14449
Mikeg1804 <121838324+Mikeg1804@users.noreply.github.com>
2023-06-21 21:16:59 -0700

    trying to fix error

---
+++ mike_germain_profile/src/App.js
@@ -0,0 +1,20 @@
+import {ResponsiveAppBar} from './components/ResponsiveAppBar';
+
+import {
+} from '@material-ui/core';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Res/>
+      <h1>Germain</h1>
+      <li>About me</li>
+      <li>Portfolio</li>
+      <li>Contact</li>
+      <li>Resume</li>
+    </div>
+  );
+}
+
+export default App;
